886f0da6f80a1a43b8ffbc3fa89d997571b5e769
Viatika265 <viatika265@gmail.com>
2025-12-18 17:56:38 +0700

    Fix TemplateSeeder column to match templates table schema

--- database/seeders/TemplateSeeder.php
+++ database/seeders/TemplateSeeder.php
@@ -2,32 +2,28 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\Template;
 
 class TemplateSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;'); 
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Template::truncate();
 
         Template::create([
-            'name' => 'Default Simple Template',
+            'title' => 'Default Simple Template',
             'description' => 'Template dasar untuk undangan resmi.',
-            'path' => 'templates.default-simple', 
+            'path' => 'templates.default-simple',
             'blade_view_name' => 'templates.default-simple',
         ]);
-        
+
         Template::create([
-            'name' => 'Premium Modern Template',
+            'title' => 'Premium Modern Template',
             'description' => 'Template modern dengan desain minimalis.',
-            'path' => 'templates.premium-modern', 
+            'path' => 'templates.premium-modern',
             'blade_view_name' => 'templates.premium-modern',
         ]);
 
